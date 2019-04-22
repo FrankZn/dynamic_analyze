36792bde9487ef5046b62f84607d5bab94417614
FrankZn <lygzzy@gmail.com>
2019-04-23 00:09:03 +0800

    Initialize package.

---
+++ src/d_roslaunch.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import roslaunch
+import rospy
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print('Lack argument.')
+        exit(1)
+
+    launchfile = argv[1]
+    print("Launching {}.".format(launchfile))
+    
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    launch = roslaunch.parent.ROSLaunchParent(uuid, [launchfile])
+    launch.start()
+    rospy.loginfo("started")
+
+    launch.shutdown()
+
+
